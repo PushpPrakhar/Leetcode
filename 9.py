879c34666de2e3deda1097190fd4e529a02617dd
PushpPrakhar <pushpprakharbhardwaj@gmail.com>
2024-08-11 18:30:36 +0530

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x<0:
+            return False
+
+        y=0
+        temp = x
+
+        while temp:
+            y= (y * 10) + (temp % 10)
+            temp //= 10
+        
+        return x==y
