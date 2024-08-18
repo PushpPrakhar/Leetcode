aeecf2176a729f7fce54fdf89f52669d6a70160f
PushpPrakhar <pushpprakharbhardwaj@gmail.com>
2024-08-18 12:08:46 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ans = 0
+        count = collections.Counter()
+
+        l = 0
+
+        for r,c in enumerate(s):
+            count[c] += 1
+            while count[c] > 1:
+                count[s[l]] -= 1
+                l += 1
+            ans = max(ans, r - l + 1)
+        return ans
