91d173445da44c2eaabf1e11c46007e03cb3cd1b
PushpPrakhar <pushpprakharbhardwaj@gmail.com>
2024-08-27 19:46:24 +0530

    Create 1509.py

---
+++ 1509.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minDifference(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 5:
+            return 0
+        
+        ans = math.inf
+
+        nums.sort()
+
+        for i in range(4):
+            ans = min(ans, nums[n - 4 + i] - nums[i])
+
+        return ans
