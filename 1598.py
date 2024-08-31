0b085f83c853b5df2a2d767d728d4b8e0232db09
PushpPrakhar <pushpprakharbhardwaj@gmail.com>
2024-08-31 19:01:55 +0530

    Create 1598.py

---
+++ 1598.py
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int ans = 0;
+
+        for (const string& log : logs){
+            if (log == "./"){
+                continue;
+            }else if(log == "../"){
+                ans = max(0, ans - 1);
+            }else{
+                ++ans;
+            }
+        }
+        return ans;
+    }
+};
