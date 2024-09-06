a4f2415f0d3ecd3031ba353851c785274192cc09
PushpPrakhar <pushpprakharbhardwaj@gmail.com>
2024-09-06 21:38:28 +0530

    Create 198.py

---
+++ 198.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        prev1 = 0
+        prev2 = 0
+
+        for num in nums:
+            dp = max(prev1, prev2 + num)
+            prev2 = prev1
+            prev1 = dp
+
+        return prev1
